6de3a55440042177b616f0256a7d88b2850401f2
Камилла Газизова <112759561+keko14@users.noreply.github.com>
2023-10-08 18:00:07 +0300

    updated hello_world.cpp to ask for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,11 @@
 include <iostream>
+include <string>
 using namespace std;
 int main() {
 cout << "Hello world!";
+string name;
+cout << "Please enter name";
+cin >> name;
+cout << "Hello world from " << name;
 }
+
